4f10c992a60e0fbfcd781fc7d0f14ff5ba075b1b
Daniel Armstrong <dany.armstrong90@gmail.com>
2022-12-17 17:22:17 +0800

    configuration change
    - linkedin url fix.
    - added screenshot.

--- packages/app/src/Constants.tsx
+++ packages/app/src/Constants.tsx
@@ -3,7 +3,7 @@ export const APP_DESCRIPTION =
   "This is zkSNARK Sudoku play and verify application.";
 
 export const GMAIL = "mailto:dany.armstrong90@gmail.com";
-export const LINKEDIN = "https://www.linkedin.com/in/web3-master/";
+export const LINKEDIN = "http://www.linkedin.com/in/daniel-armstrong90";
 export const GITHUB = "https://github.com/web3-master";
 export const GITHUB_PROJECT = "https://github.com/web3-master/zksnark-sudoku";
 
